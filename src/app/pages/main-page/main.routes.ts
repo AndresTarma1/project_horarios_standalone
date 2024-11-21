7096e25aa4b05f8daa4d9792697a8634d356fa75
AndresTarma1 <andrestormenta1@gmail.com>
2024-11-20 21:34:56 -0500

    Antes de importar otra pagina

--- src/app/pages/main-page/main.routes.ts
+++ src/app/pages/main-page/main.routes.ts
@@ -3,17 +3,6 @@ import { MainPageComponent } from "./main-page.component";
 
 export const routes: Routes = [{
   path: '', component: MainPageComponent,
-  children: [
-    {
-      path: 'academic', loadComponent: () => import('./our-academic/our-academic.component')
-    },
-    {
-      path: 'becas', loadComponent: () => import('./becas/becas.component')
-    },
-    {
-      path: 'courses', loadComponent: () => import('./courses/courses.component')
-    }
-  ]
 }];
 
 export default routes;
